ac9b09dfc015151cb3ce0e1dc2efd74653cfb0e6
chuanxiang <chxyang2023@gmail.com>
2026-08-05 20:58:25 +0800

    SAND

--- experiments/scripts/run_recon.sh
+++ experiments/scripts/run_recon.sh
@@ -6,7 +6,7 @@ for FILENAME in $DATASET_PATH/*; do
     FILENAME="$(basename "$FILENAME")"
     echo "File: ${FILENAME}"
 
-    python train_sdf.py --mesh_path $DATASET_PATH/$FILENAME --experiment_name $FILENAME --logging_root './res/Stanford' --num_steps 10000 \
+    python train_sdf.py --mesh_path $DATASET_PATH/$FILENAME --experiment_name $FILENAME --logging_root './res/Stanford' --num_steps 100000 \
         --lr 0.0001 --hidden_size 256 --hidden_layers 4  --octree_depth 8 \
         --w0 30 --fbs 1.0 \
         --gpu 4
